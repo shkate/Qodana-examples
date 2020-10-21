9baf604709e5324c00e03a54563cc70fd37dcb75
A. Kuznetsova <71134653+a-kuznetsova@users.noreply.github.com>
2020-10-21 17:35:54 +0200

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -7,11 +7,40 @@ public class HelloWorld {
     public void unusedParameter(String s) {
         if(true) {}
         System.out.println("Hello world!");
+    }public class HelloWorld {
+
+    private static String className = "HelloWorld";
+
+    public String sameReturnValueAndUnusedVariable(String s) {
+        String unusedVariable = "unusedVariable";
+        return s;
     }
     
-    public void unusedParameterWithAlwaysTrueClosure(String s) {
+    public String sameReturnValue(String s) {
+        return s;
+    }
+
+    public void unusedParameter(String s) {
         if(true) {}
         System.out.println("Hello world!");
+    }
+
+    public void unusedParameterWithAlwaysTrueClosure(String s) {
+        if(true) {}
+        System.out.println("Hello world!");    
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new HelloWorld().sameReturnValue("hi"));
+    }
+}
+
+    
+    public void unusedParameterWithAlwaysTrueClosure(String s) {
+        try {
+         if(true) {}
+            System.out.println("Hello world!");   
+        }
     }
 
     public static void main(String[] args) {
