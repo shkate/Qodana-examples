2e068d099e336501b168acf5266fb8e995786abe
A. Kuznetsova <71134653+a-kuznetsova@users.noreply.github.com>
2020-10-21 14:28:21 +0200

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -14,6 +14,11 @@ public class HelloWorld {
         System.out.println("Hello world!");
     }
 
+    public void unusedParameterWithAlwaysTrueClosure3(String s) {
+        if(true) {}
+        System.out.println("Hello world!");
+    }
+    
     public static void main(String[] args) {
         System.out.println(new HelloWorld().sameReturnValue("hi"));
     }
