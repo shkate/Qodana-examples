786922dc1edd596bce52b73fbaea4ee069efe2ff
A. Kuznetsova <71134653+a-kuznetsova@users.noreply.github.com>
2020-11-02 09:06:39 +0100

    Update TicTacToe.java

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -10,7 +10,7 @@ public class TicTacToe {
     public static void main(String[] args) throws InterruptedException {
         Scanner input = new Scanner(System.in);
         try {
-            if(true) {}
+            System.out.printf("O HAS WON!!!");
         } catch(Exception e) {}
             
         String a1 = "1";
