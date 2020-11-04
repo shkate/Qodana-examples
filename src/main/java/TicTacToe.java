ce94cbac677555d9a60f0827946905c6a2d07ebd
A. Kuznetsova <71134653+a-kuznetsova@users.noreply.github.com>
2020-11-04 09:27:47 +0100

    Update TicTacToe.java

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -10,7 +10,6 @@ public class TicTacToe {
     public static void main(String[] args) throws InterruptedException {
         Scanner input = new Scanner(System.in);
         try {
-            if(true) {}
         } catch(Exception e) {}
             
         String a1 = "1";
